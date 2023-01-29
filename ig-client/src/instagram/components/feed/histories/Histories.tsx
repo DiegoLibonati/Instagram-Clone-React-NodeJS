ffdc97e203da4959959b3d7a7db91d912451b799
DiegoLibonati <diego.libonati1998@gmail.com>
2023-01-29 01:11:19 -0300

    Histories design

--- ig-client/src/instagram/components/feed/histories/Histories.tsx
+++ ig-client/src/instagram/components/feed/histories/Histories.tsx
@@ -1,3 +1,94 @@
 export const Histories = () => {
-  return <article className="bg-white w-full h-16"></article>;
+  return (
+    <article className="bg-white w-full h-24 shadow-sm">
+      <ul className="flex items-center justify-start flex-row w-full h-full overflow-scroll overflow-y-hidden">
+        <li className="flex items-center justify-center flex-col p-2 mr-2">
+          <img
+            src="https://cdn.domestika.org/c_fill,dpr_1.0,f_auto,h_1200,pg_1,t_base_params,w_1200/v1589759117/project-covers/000/721/921/721921-original.png?1589759117"
+            alt="perfil"
+            className="w-14 h-14 rounded-full"
+          ></img>
+          <h3 className="text-gray-500 text-xs text-ellipsis w-18 overflow-hidden">
+            die_libonati
+          </h3>
+        </li>
+        <li className="flex items-center justify-center flex-col p-2 mr-2">
+          <img
+            src="https://cdn.domestika.org/c_fill,dpr_1.0,f_auto,h_1200,pg_1,t_base_params,w_1200/v1589759117/project-covers/000/721/921/721921-original.png?1589759117"
+            alt="perfil"
+            className="w-14 h-14 rounded-full"
+          ></img>
+          <h3 className="text-gray-500 text-xs text-ellipsis w-18 overflow-hidden">
+            die_libonati
+          </h3>
+        </li>
+
+        <li className="flex items-center justify-center flex-col p-2 mr-2">
+          <img
+            src="https://cdn.domestika.org/c_fill,dpr_1.0,f_auto,h_1200,pg_1,t_base_params,w_1200/v1589759117/project-covers/000/721/921/721921-original.png?1589759117"
+            alt="perfil"
+            className="w-14 h-14 rounded-full"
+          ></img>
+          <h3 className="text-gray-500 text-xs text-ellipsis w-18 overflow-hidden">
+            die_libonati
+          </h3>
+        </li>
+
+        <li className="flex items-center justify-center flex-col p-2 mr-2">
+          <img
+            src="https://cdn.domestika.org/c_fill,dpr_1.0,f_auto,h_1200,pg_1,t_base_params,w_1200/v1589759117/project-covers/000/721/921/721921-original.png?1589759117"
+            alt="perfil"
+            className="w-14 h-14 rounded-full"
+          ></img>
+          <h3 className="text-gray-500 text-xs text-ellipsis w-18 overflow-hidden">
+            die_libonati
+          </h3>
+        </li>
+
+        <li className="flex items-center justify-center flex-col p-2 mr-2">
+          <img
+            src="https://cdn.domestika.org/c_fill,dpr_1.0,f_auto,h_1200,pg_1,t_base_params,w_1200/v1589759117/project-covers/000/721/921/721921-original.png?1589759117"
+            alt="perfil"
+            className="w-14 h-14 rounded-full"
+          ></img>
+          <h3 className="text-gray-500 text-xs text-ellipsis w-18 overflow-hidden">
+            die_libonati
+          </h3>
+        </li>
+
+        <li className="flex items-center justify-center flex-col p-2 mr-2">
+          <img
+            src="https://cdn.domestika.org/c_fill,dpr_1.0,f_auto,h_1200,pg_1,t_base_params,w_1200/v1589759117/project-covers/000/721/921/721921-original.png?1589759117"
+            alt="perfil"
+            className="w-14 h-14 rounded-full"
+          ></img>
+          <h3 className="text-gray-500 text-xs text-ellipsis w-18 overflow-hidden">
+            die_libonati
+          </h3>
+        </li>
+
+        <li className="flex items-center justify-center flex-col p-2 mr-2">
+          <img
+            src="https://cdn.domestika.org/c_fill,dpr_1.0,f_auto,h_1200,pg_1,t_base_params,w_1200/v1589759117/project-covers/000/721/921/721921-original.png?1589759117"
+            alt="perfil"
+            className="w-14 h-14 rounded-full"
+          ></img>
+          <h3 className="text-gray-500 text-xs text-ellipsis w-18 overflow-hidden">
+            die_libonati
+          </h3>
+        </li>
+
+        <li className="flex items-center justify-center flex-col p-2 mr-2">
+          <img
+            src="https://cdn.domestika.org/c_fill,dpr_1.0,f_auto,h_1200,pg_1,t_base_params,w_1200/v1589759117/project-covers/000/721/921/721921-original.png?1589759117"
+            alt="perfil"
+            className="w-14 h-14 rounded-full"
+          ></img>
+          <h3 className="text-gray-500 text-xs text-ellipsis w-18 overflow-hidden">
+            die_libonati
+          </h3>
+        </li>
+      </ul>
+    </article>
+  );
 };
