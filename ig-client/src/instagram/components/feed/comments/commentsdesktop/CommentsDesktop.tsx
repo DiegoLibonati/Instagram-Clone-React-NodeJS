d495c08cbc23365dbb4433fbcf5fafca319a9e80
DiegoLibonati <diego.libonati1998@gmail.com>
2023-02-04 10:06:43 -0300

    Close modal in desktop

--- ig-client/src/instagram/components/feed/comments/commentsdesktop/CommentsDesktop.tsx
+++ ig-client/src/instagram/components/feed/comments/commentsdesktop/CommentsDesktop.tsx
@@ -1,10 +1,20 @@
+import { useContext } from "react";
+import { AiOutlineClose } from "react-icons/ai";
 import { CommentsDesktopContainer } from "./CommentsDesktopContainer";
+import { UIContext } from "../../../../../contexts/UIContext";
 
 export const CommentsDesktop = () => {
+  const { setModalClose } = useContext(UIContext);
   return (
     <div
-      className={`flex items-center justify-center flex-row bg-black bg-opacity-50 w-screen h-screen z-[999999999]`}
+      className={`flex items-center justify-center flex-row bg-black bg-opacity-50 w-screen h-screen z-[999999999] relative`}
     >
+      <AiOutlineClose
+        size={25}
+        color="white"
+        className="absolute top-2 right-2 cursor-pointer"
+        onClick={setModalClose}
+      ></AiOutlineClose>
       <CommentsDesktopContainer></CommentsDesktopContainer>
     </div>
   );
