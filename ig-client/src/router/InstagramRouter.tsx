47d20d12c293bd8946828839b59a471c6dd00c92
DiegoLibonati <diego.libonati1998@gmail.com>
2023-02-08 19:48:24 -0300

    New route for publications of profile

--- ig-client/src/router/InstagramRouter.tsx
+++ ig-client/src/router/InstagramRouter.tsx
@@ -3,6 +3,7 @@ import { LoginPage } from "../auth/pages/LoginPage";
 import { RegisterPage } from "../auth/pages/RegisterPage";
 import { HomePage } from "../instagram/pages/HomePage";
 import { ProfilePage } from "../instagram/pages/ProfilePage";
+import { PublicationsPage } from "../instagram/pages/PublicationsPage";
 
 export const InstagramRouter = () => {
   return (
@@ -15,6 +16,10 @@ export const InstagramRouter = () => {
 
       <Route path="/" element={<HomePage></HomePage>}></Route>
       <Route path="/:profile" element={<ProfilePage></ProfilePage>}></Route>
+      <Route
+        path="/:profile/:profilePublicationsId"
+        element={<PublicationsPage></PublicationsPage>}
+      ></Route>
       <Route path="/*" element={<HomePage></HomePage>}></Route>
     </Routes>
   );
