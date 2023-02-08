c76b8b7a85644007c888237c49b2905d330d6249
DiegoLibonati <diego.libonati1998@gmail.com>
2023-02-08 19:48:35 -0300

    Publications Page

---
+++ ig-client/src/instagram/pages/PublicationsPage.tsx
@@ -0,0 +1,28 @@
+import { useMediaMatch } from "../../hooks/useMediaMatch";
+import { CommentsMobile } from "../components/feed/comments/commentsmobile/CommentsMobile";
+import { Publications } from "../components/feed/publications/Publications";
+import { PublicationsHeader } from "../components/feed/publications/PublicationsHeader";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+export const PublicationsPage = () => {
+  const { matchMediaQuery } = useMediaMatch(1024);
+
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (matchMediaQuery && pathname === "/die_libonati/123123142") {
+      return navigate("/die_libonati");
+    }
+  }, [matchMediaQuery]);
+
+  return (
+    <>
+      <PublicationsHeader></PublicationsHeader>
+      <Publications></Publications>
+
+      {!matchMediaQuery && <CommentsMobile></CommentsMobile>}
+    </>
+  );
+};
