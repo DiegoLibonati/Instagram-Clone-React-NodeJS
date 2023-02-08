44b9fa3ce33f1a6905fc1c95a1a416abc04fabd3
DiegoLibonati <diego.libonati1998@gmail.com>
2023-02-08 19:48:55 -0300

    Open publications from mobile or modal for desktop

--- ig-client/src/instagram/components/profile/ProfileImage.tsx
+++ ig-client/src/instagram/components/profile/ProfileImage.tsx
@@ -1,13 +1,20 @@
 import { useContext } from "react";
 import { BsChat, BsSuitHeart } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 import { UIContext } from "../../../contexts/UIContext";
+import { useMediaMatch } from "../../../hooks/useMediaMatch";
 
 export const ProfileImage = () => {
   const { setModalOpen } = useContext(UIContext);
+  const { matchMediaQuery } = useMediaMatch(1024);
+  const navigate = useNavigate();
   return (
     <div
       className="relative group h-32 md:h-48 2xl:h-72 w-full cursor-pointer"
-      onClick={() => setModalOpen("publication")}
+      onClick={() => {
+        if (matchMediaQuery) setModalOpen("publication");
+        else navigate("/die_libonati/123123142");
+      }}
     >
       <div className="opacity-0 group-hover:opacity-75 duration-300 absolute inset-x-0 h-full flex justify-evenly items-center text-xl bg-neutral-800 text-black font-semibold">
         <p className="text-white text-center">
