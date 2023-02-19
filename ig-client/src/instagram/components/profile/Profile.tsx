5da9341554b4f4f30ea7da0f00f85918d2b0e507
Die <dlibonati@leafnoise.io>
2023-02-19 17:14:56 -0300

    fix width profile in lg and 2xl

--- ig-client/src/instagram/components/profile/Profile.tsx
+++ ig-client/src/instagram/components/profile/Profile.tsx
@@ -4,7 +4,7 @@ import { ProfileImages } from "./ProfileImages";
 
 export const Profile = () => {
   return (
-    <main className="flex items-start justify-start flex-col w-full h-full pt-14 lg:w-[80%] lg:absolute lg:right-0 lg:px-40 lg:pt-5 lg:items-center">
+    <main className="flex items-start justify-start flex-col w-full h-full pt-14 lg:w-[80%] lg:absolute lg:right-0 lg:px-20 2xl:px-40 lg:pt-5 lg:items-center">
       <ProfileHeader></ProfileHeader>
       <ProfileActions></ProfileActions>
       <ProfileImages></ProfileImages>
